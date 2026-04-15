b4ef89b205f7daa1963668b90609797b35c79c3c
SOHAM JITENDRA AMBAR <soham.ambar23@vit.edu>
2026-04-15 23:15:45 +0530

    Add emotion detection function using API

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    if response.status_code != 200:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None
+        }
+
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    return emotions
